3f7864e84ad4701f2ff77d427687672a67573997
MarioCP10 <mariocp11@correo.ugr.es>
2025-03-06 14:40:42 +0100

    s1-2

---
+++ Seminarios/hola7.asm
@@ -0,0 +1,29 @@
+pila segment stack 'stack'
+	dw 100h dup (?)
+pila ends
+datos segment 'data'
+	msg db 'hola $'
+datos ends
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
+
+		;imprimir 7 veces una cadena 
+		mov cx,0 
+		bucle: 
+			mov dx,OFFSET msg 
+			mov ah,9 
+			int 21h 
+			;actualizar contador y comprobar condición 
+			inc cx 
+			cmp cx,7
+			jne bucle 
+
+		mov ax,4C00h
+		int 21h
+	main ENDP
+codigo ends
+
+END main
